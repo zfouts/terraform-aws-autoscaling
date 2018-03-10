026d8d18aa03e8afff014b629a2a82275c3eb204
Zachary Fouts <zachary.fouts@gmail.com>
2018-03-10 14:10:23 -0600

    Enable spot pricing
    Enable spot pricing...because who wouldn't want spot pricing!

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ resource "aws_launch_configuration" "this" {
   associate_public_ip_address = "${var.associate_public_ip_address}"
   user_data                   = "${var.user_data}"
   enable_monitoring           = "${var.enable_monitoring}"
-  placement_tenancy           = "${var.placement_tenancy}"
+  #placement_tenancy           = "${var.placement_tenancy}"
   ebs_optimized               = "${var.ebs_optimized}"
   ebs_block_device            = "${var.ebs_block_device}"
   ephemeral_block_device      = "${var.ephemeral_block_device}"
@@ -23,7 +23,7 @@ resource "aws_launch_configuration" "this" {
     create_before_destroy = true
   }
 
-  # spot_price                  = "${var.spot_price}"  // placement_tenancy does not work with spot_price
+  spot_price                  = "${var.spot_price}"  // placement_tenancy does not work with spot_price
 }
 
 ####################
